48c7745c0c9d1b888e81662ac3c8d5753629fa39
Shalama <shalamafadillaa@gmail.com>
2025-07-24 13:16:07 +0700

    Create a variable
    Create it to store the reference of the squares container.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+const squareContainer = document.querySelector("#square-container");
+
 function createSquare(){
     const aSquare = document.createElement("div");
     aSquare.classList.add("square");
     return aSquare;
 }
+
