08bc58c3c1f8b3e3c587a8da4f74acf2367c4bdd
Shalama <shalamafadillaa@gmail.com>
2025-07-24 13:49:34 +0700

    Create a function to generate a grid

--- script.js
+++ script.js
@@ -7,5 +7,18 @@ function createSquare(){
 }
 
 function createGrid(num){
+    // count the number of squares to put in the container
+    const size = num * num;
     
+    // delete old grid
+    squareContainer.innerHTML = '';
+
+    // generate a grid
+    for (let i = 0; i < size; i++){
+        squareContainer.appendChild(createSquare());
+    }
+
+    // customize the container and square size
+    setContainerSize(num);
 }
+
